49314439aaa3da966d7d771c4045d5ca18d1ff96
深井隆太 <fukairyuuta@fukairyuutanoMacBook-Air.local>
2024-07-13 03:28:48 +0900

    movie

--- app/services/tmdb_client.rb
+++ app/services/tmdb_client.rb
@@ -7,13 +7,14 @@ class TmdbClient
   end
 
   # ホラー映画一覧を取得するメソッド
-  def fetch_horror_movies
+  def fetch_horror_movies(page = 1)
     options = {
       query: {
         api_key: @api_key,
         with_genres: '27',  # ホラー映画ジャンルID
         sort_by: 'popularity.desc',  # 人気順でソート
-        language: 'ja'  # 日本語のデータ
+        language: 'ja',  # 日本語のデータ
+        page: page  # ページ番号を追加
       }
     }
     self.class.get("/discover/movie", options)
